cb5cd5ab6f2143433f387fb370f7539a05ec92b3
JulioHey <juliohey.dev@gmail.com>
2021-05-30 19:59:05 -0300

    Added score and gameOver features;
    - Added Score to bottom of frame;
    - Getting input;
    - Managing moving logic;
    - Managed gameOver logic;
    - Added generateNewFruit func;
    - Added gameOver final print;

--- julio/main.cpp
+++ julio/main.cpp
@@ -12,6 +12,13 @@ enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
 
 eDirection dir;
 
+void generateNewFruit() {
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    return;
+}
+
+
 // only called one time;
 void Setup() {
     gameOver = false;
@@ -25,7 +32,8 @@ void Setup() {
 }
 
 // Draw the game on the screen
-void Draw() {
+void Draw() 
+{
     system("cls"); //system("clear")_
 
     for (int i = 0; i < width + 2; i ++) 
@@ -33,11 +41,19 @@ void Draw() {
 
     cout << endl;
 
-    for (int i = 0; i < height; i++)  {
+    for (int i = 0; i < height; i++)  
+    {
 
-        for (int j = 0; j < width; j++) {
+        for (int j = 0; j < width; j++) 
+        {
             if (j == 0 || j == width - 1)
                 cout << "#";
+            else if (i == userY && j == userX) {
+                cout << "O";
+            }
+            else if (i == fruitY && j == fruitX) {
+                cout << "F";
+            }
             else 
                 cout << " ";
         }
@@ -49,29 +65,84 @@ void Draw() {
     for (int i = 0; i < width + 2; i++) 
         cout << "#";
     cout << endl;
-
-
-
+    cout << "SCORE: " << score << endl;
 }
 
-void Input() {
+void Input() 
+{
+    if (_kbhit())
+    {
+        // Pega input
+        switch (_getch()) 
+        {
+            case 'a':
+                dir = LEFT;
+                break;
+            case 'd':
+                dir = RIGHT;
+                break;
+            case 's':
+                dir = DOWN;
+                break;
+            case 'w':
+                dir = UP;
+                break;
+            case 'x':
+                gameOver = true;
+                break;
+        }
 
+    }
 }
 
-void Logic() {
+void Logic() 
+{
+    switch (dir)
+    {
+        case LEFT:
+            userX--;
+            break;
+        case RIGHT:
+            userX++;
+            break;
+        case UP:
+            userY--;
+            break;
+        case DOWN:
+            userY++;
+            break;
+        case STOP:
+            break;
+        default:
+            break;
+    }
 
+    if (userX > width || userX < 0 || userY > height || userY < 0)
+    {
+        gameOver = true;
+    }
+
+    if (userX == fruitX && userY == fruitY)
+    {
+        score ++;
+        generateNewFruit();
+    }
 }
 
-int main() {
+int main() 
+{
     Setup();
 
     // Loop for game engine
-    while (!gameOver) {
+    while (!gameOver) 
+    {
         Draw();
         Input();
         Logic();
-        // Sleep(10);
     }
+
+    cout << "Infelizmente você perdeu...\n";
+    cout << "Você fez "<< score <<" pontos!!\n";
 
     return 0;
 }
